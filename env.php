39968b775cbbad4832092e8d34c2e8450e85f1d8
Gwoździowski, Kamil (CEPD.tech) <kamil.gwozdziowski@cepd.tech>
2024-11-08 19:23:25 +0100

    init

---
+++ env.php
@@ -0,0 +1,92 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'admin'
+    ],
+    'cache' => [
+        'graphql' => [
+            'id_salt' => 'kouHbXK3xEQ8pVAB4hPRcwsarb7te0ke'
+        ],
+        'frontend' => [
+            'default' => [
+                'id_prefix' => '69d_'
+            ],
+            'page_cache' => [
+                'id_prefix' => '69d_'
+            ]
+        ],
+        'allow_parallel_generation' => false
+    ],
+    'remote_storage' => [
+        'driver' => 'file'
+    ],
+    'queue' => [
+        'consumers_wait_for_messages' => 0,
+        'amqp' => [
+            'host' => 'rabbitmq',
+            'port' => '5672',
+            'user' => 'guest',
+            'password' => 'guest',
+            'virtualhost' => '/'
+        ]
+    ],
+    'crypt' => [
+        'key' => '0091f99685b8a2b351d0b52c20500949'
+    ],
+    'db' => [
+        'table_prefix' => '',
+        'connection' => [
+            'default' => [
+                'host' => 'db',
+                'dbname' => 'magento',
+                'username' => 'magento',
+                'password' => 'magento',
+                'model' => 'mysql4',
+                'engine' => 'innodb',
+                'initStatements' => 'SET NAMES utf8;',
+                'active' => '1',
+                'driver_options' => [
+                    1014 => false
+                ]
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'default',
+    'session' => [
+        'save' => 'files'
+    ],
+    'lock' => [
+        'provider' => 'db'
+    ],
+    'directories' => [
+        'document_root_is_pub' => true
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 1,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'compiled_config' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'full_page' => 1,
+        'config_webservice' => 1,
+        'translate' => 1
+    ],
+    'downloadable_domains' => [
+        'project.local'
+    ],
+    'install' => [
+        'date' => 'Sun, 27 Oct 2024 08:45:08 +0000'
+    ]
+];
