ee0be8a2ee8c566050adf7d60212f648144edb1f
mylek <mylek89@gmail.com>
2025-02-28 19:18:41 +0100

    Update env.php

--- env.php
+++ env.php
@@ -37,7 +37,7 @@ return [
         'table_prefix' => '',
         'connection' => [
             'default' => [
-                'host' => 'localhost',
+                'host' => '127.0.0.1',
                 'dbname' => 'magento',
                 'username' => 'magento',
                 'password' => 'magento',
